873a6267aad2b5ecbdc7c5735113af8957a5c33a
Ryan Swart <serjndestroy@gmail.com>
2011-09-23 17:21:40 +0200

    Fixed broken pattern require

--- data/godhead/scripts/content/patterns.lua
+++ data/godhead/scripts/content/patterns.lua
@@ -1,4 +1,4 @@
 for k,v in pairs(File:scan_directory("scripts/content/patterns")) do
 	require("content/patterns/" .. string.gsub(v, "([^.]*).*", "%1"))
 end
-require "content/patterns/"
+--require "content/patterns/"
